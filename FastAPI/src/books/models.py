235f368fed4cd0af77fe2cdc52fd85dacf66f9d7
Moni Chaurasiya <monichaurasiya993@gmail.com>
2026-02-06 06:23:36 +0530

    services created

--- FastAPI/src/books/models.py
+++ FastAPI/src/books/models.py
@@ -10,7 +10,7 @@ class Book(SQLModel,table=True):
             pg.UUID,
             nullable=False,
             primary_key=True,
-            default=uuid.uuid4()
+            default=uuid.uuid4
             )
     )
     title: str
@@ -19,8 +19,8 @@ class Book(SQLModel,table=True):
     published_date: str
     page_count: int
     language: str
-    created_at: datetime = Field(Column(pg.TIMESTAMP,default=datetime.now))
-    updated_at: datetime =Field(Column(pg.TIMESTAMP,default=datetime.now))
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP,default=datetime.now))
+    updated_at: datetime =Field(sa_column=Column(pg.TIMESTAMP,default=datetime.now))
     
     def __repr__(self):
         return f"<Book {self.title} >"
